0d764c914bf24ed7eb0eee928d95b155058fbc28
nithinpaulraj <paulnithin137@gmail.com>
2022-09-22 15:47:23 +0530

    task done

---
+++ src/org/Boys/GreensBoys.java
@@ -0,0 +1,29 @@
+package org.Boys;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class GreensBoys {
+	
+		public static void main(String[] args) {
+			Set<String> si=new LinkedHashSet<>();
+			si.add("nithin");
+			si.add("paul");
+			si.add("kums");
+			si.add("logs");
+			for (String string : si) {
+				System.out.println(string);
+			}
+		}
+	
+	
+		
+	
+	
+	
+		
+		
+	}
+
+
---
+++ src/org/Boys/day3.java
@@ -0,0 +1,8 @@
+package org.Boys;
+
+public class day3 {
+	public static void main(String[] args) {
+		System.out.println("task");
+	}
+
+}
